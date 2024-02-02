83053bcc522011ef02cccd1932cc387091e48daf
Dinesh Kumar <e.dinesh1991@gmail.com>
2024-02-02 10:22:49 +0000

    d

--- models/MIS-Data/mis.sql
+++
@@ -1,7 +0,0 @@
-select customerid,customername,sum(orderprofit) as totalprofit
-from 
-{{ ref('Orderfiles') }}
-group by customerid,customername
-order by totalprofit desc
-
---dadff
--- models/RAW/RAW_Customer.sql
+++
@@ -1,6 +0,0 @@
-
-
-select * from {{ ref('RAW_Customer') }}
-
-
-
